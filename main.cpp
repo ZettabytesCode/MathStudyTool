f37e2a25534b71ae6ec118b23f756420183c4ad1
Combobyte <varun.sreedhar1@gmail.com>
2023-02-07 20:12:25 -0800

    Added basic functionality of builtin commands

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include "Textbook.h"
 #include "main.h"
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
 using namespace std;
 
+unordered_map<string, command> commandMap;
+
+bool quit(vector<string> args){
+	cout << "Quiting..." << endl;
+	return false;	
+}
+
+void setup(){
+	commandMap["quit"] = quit;
+}
 
 int main(){
+	setup();
+	/*
 	vector<int> rudinProblems;
 	rudinProblems.push_back(20);
 	rudinProblems.push_back(30);
@@ -28,8 +42,31 @@ int main(){
 	
 	rudin.getShuffle(chapters);
 
+	*/
+	toolLoop();
 }
 
 void toolLoop(){
+	string line;
+	bool status = true;
+	while(status){
+		cout << "> ";
+		cin >> line;
+		status = executeCommand(line);
+	}
+}
 
+bool executeCommand(string line){
+	vector<string> parsed = parseCommand(line);
+	if(parsed.size() == 0){
+		cout << "Parse error..." << endl;
+		return true;
+	}
+	return (commandMap.find(parsed[0])->second)(parsed);
+}
+
+vector<string> parseCommand(string line){
+	vector<string> output;
+	output.push_back(line);
+	return output;
 }
--- main.h
+++ main.h
@@ -1,1 +1,12 @@
+#include <string>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+typedef function<bool(vector<string>)> command;
+
+void setup();
 void toolLoop();
+bool executeCommand(string line);
+vector<string> parseCommand(string line);
